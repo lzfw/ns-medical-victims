90538ff2c0db3dc766dbcc0086c2491a1c5d7260
gruenwald <salina.gruenwald@leopoldina.org>
2024-03-05 17:04:01 +0100

    Add Notes category for brain tissue to the Medical History view

--- nmv_list_med_hist.php
+++ nmv_list_med_hist.php
@@ -155,7 +155,7 @@ if ($victim_id) {
         SELECT h.ID_med_history_tissue id, f.tissue_form,
             s.tissue_state, h.ref_no ref_no, h.ref_no_2 ref_no_2,
             CONCAT_WS('.', IFNULL(h.since_day, '-'), IFNULL(h.since_month, '-'), IFNULL(h.since_year, '-')) date,
-            CONCAT(IFNULL(i.institution_name,'unknown'), ' - ', IFNULL(i.location, '-'), ' - ', IFNULL(c.country, '-')) AS institution
+            CONCAT(IFNULL(i.institution_name,'unknown'), ' - ', IFNULL(i.location, '-'), ' - ', IFNULL(c.country, '-')) AS institution, h.notes notes
         FROM nmv__med_history_tissue h
         LEFT JOIN nmv__tissue_form f ON f.ID_tissue_form = h.ID_tissue_form
         LEFT JOIN nmv__tissue_state s ON s.ID_tissue_state = h.ID_tissue_state
@@ -167,18 +167,19 @@ if ($victim_id) {
 
         $content .= '<h3>Brain Tissue</h3>';
         $content .= '<table class="grid">';
-        $content .= '<tr><th>Ref No.</th><th>2nd Ref No.</th><th>Date<br>(D.M.Y)</th><th>Tissue Form</th><th>Tissue State</th><th>Institution - Location</th><th>ID</th><th>Options</th>';
+        $content .= '<tr><th>Ref No.</th><th>2nd Ref No.</th><th>Date<br>(D.M.Y)</th><th>Tissue Form</th><th>Tissue State</th><th>Institution - Location</th><th>Notes</th></th><th>ID</th><th>Options</th>';
 
         $content .= '</tr>';
         while ($entry = $query->fetch_object()) {
-        	$content .= '<tr>';
-          $content .= "<td>$entry->ref_no</td>";
-          $content .= "<td>$entry->ref_no_2</td>";
-          $content .= "<td>$entry->date</td>";
-          $content .= '<td class="nowrap">'.htmlspecialchars($entry->tissue_form,ENT_HTML5).'</td>';
+            $content .= '<tr>';
+            $content .= "<td>$entry->ref_no</td>";
+             $content .= "<td>$entry->ref_no_2</td>";
+            $content .= "<td>$entry->date</td>";
+             $content .= '<td class="nowrap">'.htmlspecialchars($entry->tissue_form,ENT_HTML5).'</td>';
         	$content .= '<td class="nowrap">'.htmlspecialchars($entry->tissue_state,ENT_HTML5).'</td>';
         	$content .= '<td>'.htmlspecialchars($entry->institution,ENT_HTML5).'</td>';
-          $content .= "<td>$entry->id</td>";
+            $content .= "<td>$entry->notes</td>";
+            $content .= "<td>$entry->id</td>";
         	$content .= '<td class="nowrap">';
             $content .= createSmallButton('View Details','nmv_view_med_hist_tissue?ID_med_history_tissue='.$entry->id,'icon view');
         	if ($dbi->checkUserPermission('edit')) {
