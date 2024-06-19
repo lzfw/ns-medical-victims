bc582ed5300ccf7bf5511d7139bfbe5d85810ff7
gruenwald <salina.gruenwald@leopoldina.org>
2024-06-19 15:58:29 +0200

    typo

--- nmv_edit_victim.php
+++ nmv_edit_victim.php
@@ -65,7 +65,7 @@ $form->addField('ID_dataset_origin', SELECT)
 $form->addField(('ID_new_profile'), TEXT, 11)
     ->setLabel('ID of mpg profile (creates link)');
 $form->addField('potential_old_profile', CHECKBOX, -1)
-    ->setLabel('Potential pro-project profile');
+    ->setLabel('Potential pre-project profile');
 $form->addField('s1', STATIC_TEXT, '<br>');
 
 $form->addField('entry_status', RADIO, '', '')
